f9be53841e3b97d5e147dc93b27c85d6f291a393
Mphotle J. Marokane <josiasmarokane38@gmail.com>
2022-11-04 23:10:33 +0200

    Add the graphing plane, text input for the function and button to draw the graph.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+/*jshint esversion: 6*/
 
+init = () => {
+    draw();
+    console.log("done");
+};
+
+let draw = () => {
+    try {
+        let functionExp = math.compile(document.querySelector("#function-1").value);
+
+        let xValues = math.range(-6, 6, 0.01).toArray();
+        let yValues = xValues.map( (x) => functionExp.evaluate({x: x}));
+
+        let graphData = [{
+            x: xValues,
+            y: yValues,
+            type: 'scatter'
+        }];
+
+        Plotly.newPlot("graph", graphData);
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+document.querySelector("#functions").onsubmit = (e) => {
+    e.preventDefault();
+    draw();
+};
+
+init();
