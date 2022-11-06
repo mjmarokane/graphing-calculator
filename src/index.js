ddba60d7770dd83f62eb9857ab93337b1ede8e3c
Mphotle J. Marokane <josiasmarokane38@gmail.com>
2022-11-07 00:17:19 +0200

    add default values for the graph. add custom layout for the graph plane.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 /*jshint esversion: 6*/
 
 let numFunctions = 1;
+let defaultData = () => {
+    let xValues = math.range(domainMin, domainMax, 0.01).toArray();
+    let yValues = xValues.map( (x) => functionExp.evaluate({x: x}));
+
+    return [{
+        x: xValues,
+        y: yValues,
+        type: 'scatter'
+    }];
+};
+
+let layout = {
+    autosize: false,
+    width: 900,
+    height: 550,
+    margin: {
+      l: 50,
+      r: 50,
+      b: 35,
+      t: 10,
+      pad: 4
+    }
+  };
 
 init = () => {
-    Plotly.newPlot("graph");
+    Plotly.newPlot("graph", defaultData, layout);
 };
 
 let draw = (functionIndex) => {
@@ -30,10 +53,9 @@ let draw = (functionIndex) => {
 
 document.querySelector("#functions").onsubmit = (e) => {
     e.preventDefault();
-    Plotly.newPlot("graph");
+    Plotly.newPlot("graph", defaultData, layout);
     for (let i = 1; i <= numFunctions; i++) {
         draw(i);
-        console.log("drew");
     }
 };
 
